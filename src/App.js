119939b4417fbf4d537c54544c11cc87c19fcd9f
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-04-13 01:38:15 +0200

    Add toggling the reminder on double click and conditional styling

--- src/App.js
+++ src/App.js
@@ -26,13 +26,18 @@ const App = () => {
 
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  };
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show'}
     </div>
   );
 }
